3d5e5f646f2b0ca86d6afb53c34d9e0b09ee2903
unknown <danieldelneri@gmail.com>
2024-10-01 11:51:16 +0200

    finalmente funziona

---
+++ Assets/ShowOnlyThisClassification.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+public class ShowOnlyThisClassification : MonoBehaviour
+{
+    public ARPlaneManager planeManager;
+    public PlaneClassification classification;
+    
+    private void OnEnable(){
+        planeManager.planesChanged += SetupPlane;
+    }
+
+    private void OnDisable(){
+        planeManager.planesChanged -= SetupPlane;
+    }
+
+    private void SetupPlane(ARPlanesChangedEventArgs obj){
+        List<ARPlane> newPlane = obj.added;
+        foreach(var item in newPlane){
+            if(item.classification == classification){
+
+            }else{
+                Renderer itemRenderer = item.GetComponent<Renderer>();
+                Destroy(itemRenderer);
+            }
+        }
+    }
+}
