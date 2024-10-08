be14390441a5628968d6d2fa276939ab07b1a5a2
unknown <danieldelneri@gmail.com>
2024-10-08 16:35:36 +0200

    oo

--- Assets/UISettings.cs
+++ Assets/UISettings.cs
@@ -2,14 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class UISettings : MonoBehaviour
 {
     [SerializeField] Toggle gravityToggle;
     [SerializeField] Toggle noGravityToggle;
+
+    [SerializeField] ARPlaneManager planeManager;
+
+    [SerializeField] GameObject board;
     public void confermaImpostazioni(){
         Debug.Log("gravità:"+gravityToggle.isOn);
         SettingsManager.instance.gravity = gravityToggle.isOn;
+        
+        foreach(var plane in planeManager.trackables){
+            if(plane.classification == PlaneClassification.Table){
+                board.transform.position = plane.gameObject.transform.position;
+                board.transform.rotation = plane.gameObject.transform.rotation;
+                board.transform.Rotate(0,-90,0);
+                board.transform.position+=new Vector3(0,0.05f,0);
+            }
+        }
+
         MainManager.instance.startExperience();
         //SettingsManager.instance.spawna(); // da eliminare
     }
