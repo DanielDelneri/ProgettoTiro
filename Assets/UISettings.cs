7969427fa096d2e3f30eebcba463d06db4a63428
unknown <danieldelneri@gmail.com>
2024-10-11 13:57:21 +0200

    lll

--- Assets/UISettings.cs
+++ Assets/UISettings.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Transformers;
 
 public class UISettings : MonoBehaviour
 {
@@ -13,18 +15,38 @@ public class UISettings : MonoBehaviour
     [SerializeField] ARPlaneManager planeManager;
 
     [SerializeField] GameObject board;
-    public void confermaImpostazioni(){
+    [SerializeField] GameObject piatto;
+
+    [SerializeField] Button modificaPiattoButton;
+
+    private bool modificandoPiatto;
+
+    void Awake()
+    {
+        modificandoPiatto = false;
+    }
+
+    public void confermaImpostazioni()
+    {
 
 
-        Debug.Log("gravità:"+gravityToggle.isOn);
+        Debug.Log("gravità:" + gravityToggle.isOn);
         SettingsManager.instance.gravity = gravityToggle.isOn;
         board.SetActive(true);
-        foreach(var plane in planeManager.trackables){
-            if(plane.classification == PlaneClassification.Table){
+        foreach (var plane in planeManager.trackables)
+        {
+            if (plane.classification == PlaneClassification.Table)
+            {
                 board.transform.position = plane.gameObject.transform.position;
+                board.transform.position += new Vector3(0, 0, 0.5f);
                 board.transform.rotation = plane.gameObject.transform.rotation;
                 //board.transform.Rotate(0,180,0);
-                board.transform.position+=new Vector3(0,0.05f,0);
+                board.transform.position += new Vector3(0, 0.05f, 0);
+
+                piatto.transform.position = plane.gameObject.transform.position;
+                piatto.transform.position += new Vector3(0, 0.01f, 0);
+                piatto.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+
             }
         }
 
@@ -32,6 +54,27 @@ public class UISettings : MonoBehaviour
         //SettingsManager.instance.spawna(); // da eliminare
     }
 
-    
+    public void spostaPiatto()
+    {
+        if (modificandoPiatto)
+        {
+            ColorBlock cb = modificaPiattoButton.colors;
+            cb.normalColor = Color.blue;
+            modificaPiattoButton.colors = cb;
+            piatto.GetComponent<XRGrabInteractable>().enabled = false;
+            piatto.GetComponent<XRGeneralGrabTransformer>().enabled = false;
+        }
+        else
+        {
+            ColorBlock cb = modificaPiattoButton.colors;
+            cb.normalColor = Color.green;
+            modificaPiattoButton.colors = cb;
+            piatto.GetComponent<XRGrabInteractable>().enabled = true;
+            piatto.GetComponent<XRGeneralGrabTransformer>().enabled = true;
+        }
+
+        modificandoPiatto = !modificandoPiatto;
+    }
+
 
 }
